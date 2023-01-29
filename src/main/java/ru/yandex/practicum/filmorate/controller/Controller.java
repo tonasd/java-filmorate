78762ea37eb8c10e98c9c34c3da5bd567add4e0c
tonasd <tonasd@ya.ru>
2023-01-29 22:34:52 +0300

    refactor:change collection from List to Map

--- src/main/java/ru/yandex/practicum/filmorate/controller/Controller.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/Controller.java
@@ -3,28 +3,28 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Item;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 @Slf4j
 abstract class Controller <T extends Item> {
 
-    private final Set<T> items = new HashSet<>();
-    private int nextId = 1;
+    private final Map<Integer,T> items = new HashMap<>();
+    private int nextInt = 1;
 
     @PostMapping
     protected T create(@RequestBody T item) {
         log.info("Post {} request", item.getClass().getSimpleName());
         if (isCorrect(item)) {
-            item.setId(nextId++);
+            item.setId(nextInt);
             log.debug("Created: {}", item);
-            items.add(item);
+            items.put(nextInt++, item);
         }
         return item;
 
@@ -33,17 +33,24 @@ abstract class Controller <T extends Item> {
     @PutMapping
     protected T update(@RequestBody T item) {
         log.info("Put {} request", item.getClass().getSimpleName());
-        if (isCorrect(item)) {
+        isCorrect(item);
+        if (items.containsKey(item.getId())) {
             log.debug("Updated: {}", item);
-            items.add(item);
+        } else {
+
+            ValidationException exception = new ValidationException(String.format("%s with id %s doesn't exist",
+                    item.getClass().getSimpleName(), item.getId()));
+            log.warn("", exception);
+            throw exception;
         }
-        return item;
+
+        return items.replace(item.getId(), item);
     }
 
     @GetMapping
     protected List<T> getAll() {
         log.info("Get all {} request", this.getClass().getGenericSuperclass());
-        return List.copyOf(items);
+        return List.copyOf(items.values());
     }
 
     abstract protected boolean isCorrect(T item);
