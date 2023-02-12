8b03b2941a501f52ad4e71d6864ddb9e831cdccf
tonasd <tonasd@ya.ru>
2023-02-12 12:57:59 +0300

    ready for review sprint10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
-import javax.validation.constraints.Positive;
 import java.util.List;
 
 @Slf4j
@@ -38,27 +37,26 @@ public class FilmController{
     }
 
     @GetMapping(value = "/{id}")
-    public Film getFilm(@PathVariable @Positive Long id) {
+    public Film getFilm(@PathVariable Long id) {
         final Film film = service.get(id);
         log.info("Given film: {}", film);
         return film;
     }
 
     @PutMapping(path = "/{id}/like/{userId}")
-    public void addLike(@PathVariable(name = "id") @Positive Long filmId, @PathVariable @Positive Long userId) {
+    public void addLike(@PathVariable(name = "id") Long filmId, @PathVariable Long userId) {
         service.addLike(filmId, userId);
         log.info("Film {} is liked by user {}", filmId, userId);
     }
 
     @DeleteMapping(path = "/{id}/like/{userId}")
-    public void removeLike(@PathVariable(name = "id") @Positive Long filmId, @PathVariable @Positive Long userId) {
+    public void removeLike(@PathVariable(name = "id") Long filmId, @PathVariable Long userId) {
         service.removeLike(filmId, userId);
         log.info("Film {} is unliked by user {}", filmId, userId);
     }
 
     @GetMapping(path = "/popular")
-    public List<Film> getPopular(@RequestParam(required = false, defaultValue = "10", value = "count")
-                                     @Positive Integer size) {
+    public List<Film> getPopular(@RequestParam(required = false, defaultValue = "10", value = "count") Integer size) {
         final List<Film> popularList = service.getPopular(size);
         log.info("Get {} popular films request. Given {} films", size, popularList.size());
         return popularList;
