87567f90288c0ee262f6c577510864665bc5ef30
tonasd <tonasd@ya.ru>
2023-02-12 12:57:59 +0300

    ready for review sprint10

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
-import javax.validation.constraints.Positive;
 import java.util.List;
 
 @Slf4j
@@ -38,33 +37,33 @@ public class UserController{
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable @Positive Long id) {
+    public User getUser(@PathVariable Long id) {
         final User user = service.get(id);
         log.info("Given user: {}", user);
         return user;
     }
 
     @PutMapping("/{id}/friends/{friendId}")
-    public void addFriend(@PathVariable @Positive Long id, @PathVariable @Positive Long friendId) {
+    public void addFriend(@PathVariable Long id, @PathVariable Long friendId) {
         service.addFriend(id, friendId);
         log.info("Users {} and {} now friends", id, friendId);
     }
 
     @DeleteMapping("{id}/friends/{friendId}")
-    public void removeFriend(@PathVariable @Positive Long id, @PathVariable @Positive Long friendId) {
+    public void removeFriend(@PathVariable Long id, @PathVariable Long friendId) {
         service.removeFriend(id, friendId);
         log.info("Users {} and {} not friends anymore", id, friendId);
     }
 
     @GetMapping("/{id}/friends")
-    public List<User> getFriends(@PathVariable @Positive Long id) {
+    public List<User> getFriends(@PathVariable Long id) {
         final List<User> friends = service.getFriends(id);
         log.info("Returned list of {} friends for user {}", friends.size(), id);
         return friends;
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends(@PathVariable @Positive Long id, @PathVariable @Positive Long otherId) {
+    public List<User> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
         final List<User> commonFriends = service.commonFriends(id, otherId);
         log.info("Returned list of {} common friends for users {} and {}", commonFriends.size(), id, otherId);
         return commonFriends;
