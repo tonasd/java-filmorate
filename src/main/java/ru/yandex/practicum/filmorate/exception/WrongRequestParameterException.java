94ab51e39818dd284b87151deb93d746672afc1f
tonasd <tonasd@ya.ru>
2023-04-06 08:00:51 +0300

    style: fix2

--- src/main/java/ru/yandex/practicum/filmorate/exception/WrongRequestParameterException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/WrongRequestParameterException.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class WrongRequestParameterException extends RuntimeException{
-    public WrongRequestParameterException (String description) {super(description);}
+public class WrongRequestParameterException extends RuntimeException {
+    public WrongRequestParameterException(String description) {
+        super(description);
+    }
 }
